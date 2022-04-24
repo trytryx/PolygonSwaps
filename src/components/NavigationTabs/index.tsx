edfa331955058a6537c0a20484317ce5daebbcb5
trytryx <100495047+trytryx@users.noreply.github.com>
2022-04-25 00:23:46 +0300

    Update index.tsx

--- src/components/NavigationTabs/index.tsx
+++ src/components/NavigationTabs/index.tsx
@@ -106,11 +106,11 @@ export const Nav = ({ activeIndex = 0 }: { activeIndex?: number }) => (
     <ButtonMenuItem
       id="pool-nav-link"
       as="a"
-      href="https://www.binance.org/en/panama"
+      href="https://polyowl-frontend-main.vercel.app/"
       target="_blank"
       rel="noreferrer noopener"
     >
-      Bridge
+      HyperSwapp
     </ButtonMenuItem>
   </ButtonMenu>
 )
